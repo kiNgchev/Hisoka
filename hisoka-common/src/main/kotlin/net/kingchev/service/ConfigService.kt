c505218adfbf8741add64cfccb8b89ffbf294145
KiNgchev <kingchev.net@gmail.com>
2024-11-20 23:12:54 +0300

    Update ConfigServiceg

--- hisoka-common/src/main/kotlin/net/kingchev/service/ConfigService.kt
+++ hisoka-common/src/main/kotlin/net/kingchev/service/ConfigService.kt
@@ -3,6 +3,8 @@ package net.kingchev.service
 import net.kingchev.extensions.toIntOrDef
 import net.kingchev.model.HisokaConfig
 import net.kingchev.structure.Initializable
+import net.kingchev.structure.delegetion.Environment
+import net.kingchev.structure.delegetion.Property
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
@@ -14,10 +16,10 @@ public object ConfigService : Initializable {
     public override fun initialize() {
         logger.info("Config initialization started")
 
-        val token = System.getenv("BOT_TOKEN")
-        val totalShards = System.getenv("TOTAL_SHARDS") ?: System.getProperty("total.shards", "1")
+        val token by Environment("BOT_TOKEN")
+        val totalShards by Property("shards.total")
 
-        config = HisokaConfig(token = token, totalShards = totalShards.toIntOrDef())
+        config = HisokaConfig(token = token, totalShards = totalShards.toIntOrDef(1))
         logger.info("Config initialization completed")
     }
 }
