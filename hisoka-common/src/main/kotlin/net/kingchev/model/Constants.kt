3819d3aa8a97e60764d55dc12fc1395894545256
KiNgchev <kingchev.net@gmail.com>
2024-11-21 22:15:28 +0300

    Add Constants

---
+++ hisoka-common/src/main/kotlin/net/kingchev/model/Constants.kt
@@ -0,0 +1,6 @@
+package net.kingchev.model
+
+public object Constants {
+    public const val ID_STRING: String   = "1271073974356279396"
+    public const val ID_LONG: Long       = 1271073974356279396
+}
