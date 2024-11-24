1754f69f1e34ed45a4ba44b94f607bbdc29e115c
KiNgchev <kingchev.net@gmail.com>
2024-11-24 15:50:27 +0300

    Update Constants
    Changes:
    - Added bot username constant

--- hisoka-common/src/main/kotlin/net/kingchev/model/Constants.kt
+++ hisoka-common/src/main/kotlin/net/kingchev/model/Constants.kt
@@ -3,4 +3,6 @@ package net.kingchev.model
 public object Constants {
     public const val ID_STRING: String   = "1271073974356279396"
     public const val ID_LONG: Long       = 1271073974356279396
+
+    public const val BOT_USERNAME: String = "Hisoka Morow#6440"
 }
