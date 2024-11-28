57c0f3f8e9d8a48fb781126f66b7ff0961042299
KiNgchev <kingchev.net@gmail.com>
2024-11-28 23:13:41 +0300

    Updated constants

--- hisoka-common/src/main/kotlin/net/kingchev/model/Constants.kt
+++ hisoka-common/src/main/kotlin/net/kingchev/model/Constants.kt
@@ -1,8 +1,17 @@
 package net.kingchev.model
 
-public object Constants {
+import dev.kord.common.Color
+
+public object BotMetadata {
     public const val ID_STRING: String   = "1271073974356279396"
     public const val ID_LONG: Long       = 1271073974356279396
 
     public const val BOT_USERNAME: String = "Hisoka Morow#6440"
 }
+
+public object Colors {
+    public val Purple: Color = Color(0x7567ff)
+    public val Critical: Color = Color(0xff0900)
+    public val Red: Color = Color(0xDC143C)
+}
+
