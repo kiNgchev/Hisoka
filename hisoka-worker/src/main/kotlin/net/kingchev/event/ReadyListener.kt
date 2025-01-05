6a6f66c068571a24bd95539b0b41a82e2b5db0d9
KiNgchev <kingchev.net@gmail.com>
2025-01-05 20:21:19 +0300

    feat(events): add ready event listener

---
+++ hisoka-worker/src/main/kotlin/net/kingchev/event/ReadyListener.kt
@@ -0,0 +1,24 @@
+package net.kingchev.event
+
+import dev.kord.common.entity.PresenceStatus
+import dev.kord.core.Kord
+import dev.kord.core.entity.Emoji
+import dev.kord.core.entity.StandardEmoji
+import dev.kord.core.event.gateway.ReadyEvent
+import dev.kord.core.on
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+public class ReadyListener(kord: Kord) : IListener {
+    private val logger: Logger = LoggerFactory.getLogger(ReadyListener::class.java)
+
+    init {
+        kord.on<ReadyEvent> {
+            kord.editPresence {
+                status = PresenceStatus.DoNotDisturb
+                playing("Покер\uD83C\uDCCF")
+            }
+            logger.info("Bot was started with id[${kord.selfId}], name[${kord.getSelf().globalName}]")
+        }
+    }
+}
